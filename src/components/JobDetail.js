47419d6f2b441b3d3ba3503474562ac94830fdc9
Jon Dale <jonners@gmail.com>
2016-07-22 22:34:02 -0400

    Added more info on job detail page

--- src/components/JobDetail.js
+++ src/components/JobDetail.js
@@ -25,11 +25,26 @@ export default class JobDetail extends Component {
             if(this.props.job) {
               return (
                 <div>
-                  <h2 className="red h1">{this.props.job.title}</h2>
+                  <h2 className="blue h1 line-height-1">{this.props.job.title}</h2>
+                  <div className="flex">
+                    <div className="flex-grow">
+                      <p className="h4 mt0 mb1">{this.props.job.company} | {this.props.job.location}</p>
+                      <p className="gray h5 mt1 mb3">{this.props.job.date}</p>
+                    </div>
+                    <div>
+                      <a href={this.props.job.url}>View job on {this.props.job.provider}</a>
+                    </div>
+                  </div>
                   <div dangerouslySetInnerHTML={this.getDescription()} />
                   <div dangerouslySetInnerHTML={this.getApply()} />
                 </div>
               );
+            }
+            else {
+              return (
+                <p style={{top: '50%'}}
+                   className="absolute right-0 left-0 center caps silver">No Job Selected</p>
+              )
             }
           })()
         }
