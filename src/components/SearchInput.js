97ce0e00859ef938feeb4afc05a4ffdd511a8ae0
Jon Dale <jonners@gmail.com>
2016-07-22 22:33:32 -0400

    Added SVG serch icon

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,5 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import ReactDom from 'react-dom';
+import searchSvg from '../images/search.svg';
 
 export default class SearchInput extends Component {
   static propTypes = {
@@ -7,35 +8,46 @@ export default class SearchInput extends Component {
     status: PropTypes.string,
   };
 
-  searchJob(event) {
-    if (event.which === 13) {
-      const keyword = ReactDom.findDOMNode(this.refs.keyword).value;
-      const location = ReactDom.findDOMNode(this.refs.location).value;
-      this.props.actions.searchJobAction(keyword, location);
-    }
+  searchKeyDown(event) {
+    if (event.which === 13) searchJob();
   }
+
+  searchJob() {
+    const keyword = ReactDom.findDOMNode(this.refs.keyword).value;
+    const location = ReactDom.findDOMNode(this.refs.location).value;
+    this.props.actions.searchJobAction(keyword, location);
+  }
+
   render() {
+    var searchStyle = {
+      backgroundImage: 'url(' + searchSvg + ')',
+      backgroundSize: '20px',
+      backgroundRepeat: 'no-repeat',
+      width: '50px',
+      backgroundPosition: '55% 50%',
+      cursor: 'pointer'
+    }
     return (
       <div className="clearfix flex flex-stretch">
         <div className="flex-auto">
           <div className="col col-6 border-left">
-            <input onKeyDown={this.searchJob.bind(this)}
+            <input onKeyDown={this.searchKeyDown.bind(this)}
                    type="text"
                    ref="keyword"
                    className="field col-12"
                    placeholder="Job Title, Keyword, or Company" />
           </div>
           <div className="col col-6 border-left">
-            <input onKeyDown={this.searchJob.bind(this)}
+            <input onKeyDown={this.searchKeyDown.bind(this)}
                    type="text"
                    ref="location"
                    className="field col-12"
                    placeholder="City, Country, Zip Code, or Remote" />
           </div>
         </div>
-        <div className="flex-none bg-red white">
-          search button
-        </div>
+        <div className="flex-none bg-black black block"
+             onClick={this.searchJob.bind(this)}
+             style={searchStyle}>&nbsp;</div>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,26 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  debug: true,
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     './src/index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'app.js',
-    publicPath: '/static/',
+    filename: 'static/app.js',
+    publicPath: '/',
   },
   module: {
     loaders: [
-      {test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader',},
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader',
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file?name=[path][name].[ext]&context=./app/static',
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
